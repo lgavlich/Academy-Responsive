70614d2e2f4c859c5ddd283562f10457c95d65fb
lgavlich <lgavlich@gmail.com>
2022-08-25 12:46:10 +0300

    commit

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Button } from 'antd'
 import Bitmap from '../images/Bitmap.png';
 import students from '../images/students.png'
 import lesson from '../images/lesson.png';
+import inform from '../images/inform.png'
+import math2 from '../images/math2.png'
+import math3 from '../images/math3.png'
+import chem from '../images/chem.png'
 import Alisia from '../images/Alisia.png';
 import Ohta from '../images/Ohta.png'
+
 
 import s from './Courses.module.css'
 
@@ -18,17 +24,93 @@ export default function Courses() {
           </div>
           <div style={{ display: "flex" }}>
             <img style={{ width: "35rem", height: "26rem" }} src={Bitmap} />
-            <li className={s.card}>
+            <li style={{ marginLeft: "1.8rem" }} className={s.card}>
               <img style={{ width: "16rem", height: "11rem" }} src={students} />
               <p>Sed do eiusmod tempor</p>
               <img src={Ohta} />
               <p>Ohta Kin</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
             </li>
-            <li className={s.card}>
+            <li style={{ marginLeft: "1.8rem" }} className={s.card}>
               <img style={{ width: "16rem", height: "11rem" }} src={lesson} />
               <p>Labore et dolore magna</p>
               <img src={Alisia} />
               <p>Alicia Puma</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
+            </li>
+          </div>
+          <div
+            style={{ display: "flex", flexWrap: "wrap", paddingTop: "2rem" }}
+          >
+            <li className={s.card}>
+              <img style={{ width: "16rem", height: "11rem" }} src={inform} />
+              <p>Commodo consequat</p>
+              <img src={Alisia} />
+              <p>Alicia Puma</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
+            </li>
+            <li style={{ marginLeft: "1.8rem" }} className={s.card}>
+              <img style={{ width: "16rem", height: "11rem" }} src={math2} />
+              <p>Ullamco laboris nisi ut aliquip</p>
+              <img src={Alisia} />
+              <p>Alicia Puma</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
+            </li>
+            <li style={{ marginLeft: "1.8rem" }} className={s.card}>
+              <img style={{ width: "16rem", height: "11rem" }} src={chem} />
+              <p>Quis nostrud exercitation</p>
+              <img src={Alisia} />
+              <p>Alicia Puma</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
+            </li>
+            <li style={{ marginLeft: "1.8rem" }} className={s.card}>
+              <img style={{ width: "16rem", height: "11rem" }} src={math3} />
+              <p>Enim ad minim veniam</p>
+              <img src={Alisia} />
+              <p>Alicia Puma</p>
+              <Button
+                style={{
+                  border: "1px solid rgba(74, 144, 226, 0.3);",
+                  borderRadius: "6px",
+                }}
+              >
+                START NOW
+              </Button>
             </li>
           </div>
         </Container>
